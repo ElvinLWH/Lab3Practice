6d4fee4e01fe53d0641884162fb49ef84958020e
ElvinLWH <elvinlawenghoe1@gmail.com>
2023-11-22 22:13:01 +0800

    Added exercise 2

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -28,3 +28,30 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_greater_than_or_equal_to_ten():
+    result = []
+    input_arr = [12, 23, 45, 54, 67, 1, 3, 5, 6, 7]
+    expected_result = 1
+
+    result =Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == expected_result)
+
+def test_zero_numbers_entered():
+    result = []
+    input_arr = []
+    expected_result = 0
+
+    result =Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == expected_result)
+
+def test_not_integer_values():
+    result = []
+    input_arr = [12.1, 23, 45, 54, 67, 1, 3, 5]
+    expected_result = 2
+
+    result =Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == expected_result)
